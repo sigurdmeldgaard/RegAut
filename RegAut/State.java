f24dc3dd4ec99662613e270a4b55604874fe5257
stm <sigurdmeldgaard@dhcp-11-64-114.daimi.au.dk>
2009-01-13 13:24:34 +0100

    Begyndt paa opgaver til forste uge

---
+++ RegAut/State.java
@@ -0,0 +1,31 @@
+package RegAut;
+
+/** An automaton state. */
+public class State implements Cloneable {
+    
+    /** State name. (The state name is only for printing and has no formal meaning.) */
+    public String name;
+    
+    /** Constructs a new state with no name. */
+    public State() {
+        name = "";
+    }
+    
+    /** 
+     * Constructs a new state with the given name. 
+     * @param name state name
+     */
+    public State(String name) {
+        this.name = name;
+    }
+    
+    /** Clones this state. */
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
